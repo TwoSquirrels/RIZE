04c0c7bb1f3102e4cb3622ec5c2eedbc429a036a
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-09-17 23:42:02 +0900

    token 周りをリファクタリング

--- src/token.js
+++ src/token.js
@@ -13,20 +13,16 @@ function updateRizeTokenById(userId) {
   const lineName = fetchLineName(userId);
   const newRizeToken = Utilities.getUuid();
   const newDeadline = Date.now() + 1000 * 300000; // 5 分後
-  const tableBottom = ACCESS_TOKEN_SHEET.getLastRow();
-  if (tableBottom <= 1) {
-    ACCESS_TOKEN_SHEET.appendRow([userId, lineName, newRizeToken, newDeadline]);
-    return newRizeToken;
-  }
-  const table = ACCESS_TOKEN_SHEET.getSheetValues(2, 1, tableBottom - 1, 4);
-  const rowIndex = table.findIndex((row) => row[Column.USER_ID - 1] == userId);
-  if (rowIndex === -1) {
+  const rows = ACCESS_TOKEN_SHEET.getDataRange().getValues();
+  const rowNumber =
+    rows.slice(1).findIndex((row) => row[Column.USER_ID - 1] == userId) + 2;
+  if (rowNumber === 1) {
     ACCESS_TOKEN_SHEET.appendRow([userId, lineName, newRizeToken, newDeadline]);
   } else {
-    ACCESS_TOKEN_SHEET.getRange(rowIndex + 2, Column.ACCESS_TOKEN).setValue(
+    ACCESS_TOKEN_SHEET.getRange(rowNumber, Column.ACCESS_TOKEN).setValue(
       newRizeToken
     );
-    ACCESS_TOKEN_SHEET.getRange(rowIndex + 2, Column.DEADLINE).setValue(
+    ACCESS_TOKEN_SHEET.getRange(rowNumber, Column.DEADLINE).setValue(
       newDeadline
     );
   }
@@ -34,21 +30,18 @@ function updateRizeTokenById(userId) {
 }
 
 function updateRizeTokenByToken(token) {
-  const tableBottom = ACCESS_TOKEN_SHEET.getLastRow();
-  if (tableBottom <= 1) throw new Error("No token found.");
-  const table = ACCESS_TOKEN_SHEET.getSheetValues(2, 1, tableBottom - 1, 4);
-  const rowIndex = table.findIndex(
-    (row) => row[Column.ACCESS_TOKEN - 1] == token
-  );
-  if (rowIndex === -1) throw new Error("No token found.");
-  const userId = table[rowIndex][Column.USER_ID - 1];
-  const displayName = table[rowIndex][Column.DISPLAY_NAME - 1];
+  const rows = ACCESS_TOKEN_SHEET.getDataRange().getValues();
+  const rowNumber =
+        rows.slice(1).findIndex((row) => row[Column.ACCESS_TOKEN - 1] == token) + 2;
+  if (rowNumber === 1) throw new Error("Invalid token.");
+  const userId = rows[rowNumber - 1][Column.USER_ID - 1];
+  const displayName = rows[rowNumber - 1][Column.DISPLAY_NAME - 1];
   const newRizeToken = Utilities.getUuid();
   const newDeadline = Date.now() + 1000 * 600000; // 10 分後
-  ACCESS_TOKEN_SHEET.getRange(rowIndex + 2, Column.ACCESS_TOKEN).setValue(
+  ACCESS_TOKEN_SHEET.getRange(rowNumber, Column.ACCESS_TOKEN).setValue(
     newRizeToken
   );
-  ACCESS_TOKEN_SHEET.getRange(rowIndex + 2, Column.DEADLINE).setValue(
+  ACCESS_TOKEN_SHEET.getRange(rowNumber, Column.DEADLINE).setValue(
     newDeadline
   );
   return { userId, displayName, rizeToken: newRizeToken };
