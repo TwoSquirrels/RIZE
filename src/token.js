80070643cf1b087d565e3529ef7983dc9ecd1d3a
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2022-09-17 16:36:43 +0900

    updateRizeToken 多分完成

--- src/token.js
+++ src/token.js
@@ -1,9 +1,35 @@
 // SPDX-License-Identifier: MIT
 
+const ACCESS_TOKEN_SHEET =
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName("ACCESS TOKEN");
+const Column = {
+  USER_ID: 1,
+  DISPLAY_NAME: 2,
+  ACCESS_TOKEN: 3,
+  DEADLINE: 4,
+};
+
 function updateRizeTokenById(userId) {
   const lineName = fetchLineName(userId);
   const newRizeToken = Utilities.getUuid();
-  // TODO: update spreadsheet
+  const newDeadline = Date.now() + 1000 * 300000; // 5 分後
+  const table = ACCESS_TOKEN_SHEET.getSheetValues(
+    2,
+    1,
+    ACCESS_TOKEN_SHEET.getLastRow() - 1,
+    4
+  );
+  const rowIndex = table.findIndex((row) => row[Column.USER_ID] === userId);
+  if (rowIndex === -1) {
+    ACCESS_TOKEN_SHEET.appendRow([userId, lineName, newRizeToken, newDeadline]);
+  } else {
+    ACCESS_TOKEN_SHEET.getRange(rowIndex + 2, Column.ACCESS_TOKEN).setValue(
+      newRizeToken
+    );
+    ACCESS_TOKEN_SHEET.getRange(rowIndex + 2, Column.DEADLINE).setValue(
+      newDeadline
+    );
+  }
   return newRizeToken;
 }
 
